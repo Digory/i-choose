ad3f34df971bda4e6fd1902d64276aa21423dcd4
Digory <digory1@googlemail.com>
2018-07-15 19:14:09 +0100

    Initial commit

---
+++ app/src/main/java/digory/ichoose/MainActivity.java
@@ -0,0 +1,28 @@
+package digory.ichoose;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton foodAndDrinkButton = (ImageButton) findViewById(R.id.foodAndDrinkImageButton);
+        foodAndDrinkButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
+                startActivity(startIntent);
+
+            }
+        });
+    }
+}
