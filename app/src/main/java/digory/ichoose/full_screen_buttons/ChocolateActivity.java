e750732f5e09b430e6118f8c25d9afc0204f9f9f
Digory <digory1@googlemail.com>
2018-07-29 18:08:21 +0100

    Add large chocolate button and corresponding WAV

--- app/src/main/java/digory/ichoose/full_screen_buttons/ChocolateActivity.java
+++ app/src/main/java/digory/ichoose/full_screen_buttons/ChocolateActivity.java
@@ -1,28 +1,26 @@
-package digory.ichoose;
+package digory.ichoose.full_screen_buttons;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-import digory.ichoose.full_screen_buttons.ChocolateActivity;
+import digory.ichoose.R;
 
-public class SnackActivity extends AppCompatActivity {
+public class ChocolateActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_snack);
+        setContentView(R.layout.activity_chocolate);
 
         ImageButton chocolateButton = (ImageButton) findViewById(R.id.chocolateBtn);
+        final MediaPlayer playChocolate = MediaPlayer.create(this, R.raw.chocolate);
         chocolateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent startIntent = new Intent(getApplicationContext(), ChocolateActivity.class);
-                startActivity(startIntent);
+                playChocolate.start();
             }
         });
     }
