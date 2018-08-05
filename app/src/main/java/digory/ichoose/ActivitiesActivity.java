69e1b64c034d7f01dfc3c335df1fad0c2efe12bf
Digory <digory1@googlemail.com>
2018-08-05 17:14:51 +0100

    Fix button layout of FoodAndDrinkActivity landscape mode

--- app/src/main/java/digory/ichoose/ActivitiesActivity.java
+++ app/src/main/java/digory/ichoose/ActivitiesActivity.java
@@ -1,7 +1,11 @@
 package digory.ichoose;
 
+import android.app.Application;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ActivitiesActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class ActivitiesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activities);
+
+        ImageButton bookButton = (ImageButton) findViewById(R.id.bookBtn);
+        bookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent startIntent = new Intent(getApplicationContext(), BookActivity.class);
+                startActivity(startIntent);
+            }
+        });
     }
 }
