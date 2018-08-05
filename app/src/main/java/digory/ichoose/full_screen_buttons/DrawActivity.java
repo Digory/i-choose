ca56d918f23527d032af833d7b764c46d2a01167
Digory <digory1@googlemail.com>
2018-08-05 18:01:29 +0100

    Add large draw button and corresponding WAV

--- app/src/main/java/digory/ichoose/full_screen_buttons/DrawActivity.java
+++ app/src/main/java/digory/ichoose/full_screen_buttons/DrawActivity.java
@@ -1,7 +1,10 @@
 package digory.ichoose.full_screen_buttons;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import digory.ichoose.R;
 
@@ -11,5 +14,14 @@ public class DrawActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_draw);
+
+        ImageButton drawButtonLarge = (ImageButton) findViewById(R.id.drawBtnLarge);
+        final MediaPlayer playDraw = MediaPlayer.create(this, R.raw.draw);
+        drawButtonLarge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playDraw.start();
+            }
+        });
     }
 }
